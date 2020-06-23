d9d0051c2b43d757aacded4995cf756051e9e545
Donovan Costa <donovancosta@gmail.com>
2020-06-23 23:57:23 +0100

    - Added Call To Action and About components - Added Call To Action background image to public folder

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,34 @@
+<template>
+    <div
+        class="cta">
+        <b-container>
+            <b-row justify align-v="center" align-h="center" >
+                <h1>{{message}}</h1>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CallToAction",
+    props: {
+        message: String
+    }
+}
+</script>
+
+<style>
+    .cta {
+        min-height: 100vh;
+        display: flex;
+        position: relative;
+        justify-content: center;
+        align-items: center;
+        background-image: url('/img/tianyi-ma-WiONHd_zYI4-unsplash.jpg');
+        background-size:cover;
+        background-position: center;
+        color: white;
+        font-family: 'Roboto';
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <Navbar />
+    <CallToAction message="A developer with a thirst for knowledge and ambition to get things done." />
+    <About />
     <Footer 
         siteName="donovancosta.com"
         />
@@ -11,12 +13,16 @@
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
+import About from '@/components/About.vue'
+import CallToAction from '@/components/CallToAction.vue'
 
 export default {
   name: 'Home',
   components: {
     Navbar,
     Footer,
+    About,
+    CallToAction
   }
 }
 </script>
