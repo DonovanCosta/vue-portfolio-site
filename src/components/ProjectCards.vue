f2d2b3133fe32a6070ec0bff72d7f142c4941ac4
Donovan Costa <donovancosta@gmail.com>
2020-07-09 22:54:59 +0100

    - Restructured project card - Content is now added dynamically through the use of props.

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -1,22 +1,22 @@
 <template>
-    <b-card
-        title="Card Title"
-        img-src="https://picsum.photos/600/300/?image=25"
-        img-alt="Image"
-        img-top
-        tag="article"
-        class="mb-4"
-    >
-        <b-card-text>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-        </b-card-text>
-        <b-button href="#" variant="primary">Go somewhere</b-button>
-    </b-card>
+    <div>
+        <b-card no-body style="max-width: 20rem;" class="mb-4" >
+            <b-card-img :src="src" alt="Image"></b-card-img>
+            <b-card-body>
+                <b-card-title>{{title}}</b-card-title>
+                <b-card-text v-html="short_description"></b-card-text>
+                <b-button variant="primary" :to="{ name: 'ProjectDetailView', params:  {id: project_id} }" class="card_btn">
+                    Read more   
+                </b-button>   
+            </b-card-body>
+        </b-card>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "ProjectCards"
+    name: "ProjectCards",
+    props: ['title', 'src', 'short_description', 'project_id']
 }
 </script>
 
@@ -25,6 +25,21 @@ export default {
 .card {
     width: 400px;
     height: 400px;
+    border-radius: 5px;
 }
+/* 
+.card_btn {
+    color: #4fbfa8;
+    background: none;
+    border: 2px solid #4fbfa8;
+}
+
+.card_btn:hover {
+    color: #ffffff;
+    background: #4fbfa8;
+    border: 2px solid #4fbfa8;
+    transition:  0.3s ease-in;
+} */
+
 
 </style>
