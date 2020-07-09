99824c9f319b6b86eb55e6deb8514cc29a71dc56
Donovan Costa <donovancosta@gmail.com>
2020-07-09 22:50:41 +0100

    - Added props to the About component - Updated about and experience paragraphs to dynamic text

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,20 @@
 <template>
-    <div class="about">
+    <div id="about" class="about">
         <b-container>
             <b-row justify align-v="center" align-h="center" >
                 <b-col cols="12" class="about_text">
-                     <h1>About me</h1>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, totam vitae. Quia, esse aliquid iure obcaecati nemo nobis, possimus placeat amet explicabo illum dolor impedit et mollitia iusto in optio? Earum magnam quasi dolor odio doloribus eum, officiis asperiores consectetur enim eveniet unde velit numquam. Enim esse beatae debitis et neque? Qui perferendis enim laborum aliquid consequuntur sequi repudiandae? Provident, quidem voluptatem. Praesentium voluptatum id corrupti eaque accusamus quasi eveniet esse vitae tempore beatae quam, magni, sequi quod. Recusandae placeat cumque nulla neque culpa perspiciatis sequi rerum voluptatem id fugit? Aperiam optio nisi maiores vitae temporibus accusamus tempora consequatur nemo!
-                    </p>
-                    </b-col>
+                    <h1>About me</h1>
+                    <div v-html="about_me"></div>
+                </b-col>
             </b-row>
+             <hr>
             <b-row>
                 <b-col cols="12" class="experiences">
                     <h1>experience</h1>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sapiente quos accusamus praesentium atque neque qui. Velit nihil tempora fuga repellat labore suscipit delectus asperiores ipsam ad, magni a aut molestiae totam eos in consequatur eligendi voluptatum iure omnis dignissimos natus possimus cupiditate? Omnis dolor nesciunt nisi quae suscipit et.
-                    </p>
+                    <div v-html="experience"></div>
                 </b-col>
             </b-row>
+            <hr>
         </b-container>
     </div>
 </template>
@@ -24,11 +22,7 @@
 <script>
 export default {
     name: "About",
-    props: {
-        message: String
-    }
-
-
+    props: ['about_me', 'experience' ]
 }
 </script>
 
