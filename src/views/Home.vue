ade401c507b73b91adf3373be75e99289b4b6664
Donovan Costa <donovancosta@gmail.com>
2020-07-09 22:59:55 +0100

    - Added axios library for data requests - ProjectDetail component updated to display fetched data.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="home">
     <CallToAction message="A developer with a thirst for knowledge and ambition to get things done." />
-    <About />
-    <ProjectList />
+    <About 
+        :about_me="bio.about_me"
+        :experience="bio.experience"
+    />
+    <ProjectList 
+        :project-list="projects"
+    />
   </div>
 </template>
 
@@ -12,12 +17,39 @@ import About from '@/components/About.vue'
 import CallToAction from '@/components/CallToAction.vue'
 import ProjectList from '@/components/ProjectList.vue'
 
+import axios from 'axios'
+
 export default {
   name: 'Home',
+  data(){
+      return{
+          projects: [],
+          bio:[]
+      }
+  },
   components: {
     About,
     CallToAction,
     ProjectList
+  },
+  //lifecycle hook
+   created(){
+
+        function getBiography() {
+            return axios.get('http://127.0.0.1:8000/api/bio/');
+        }
+
+        function getProjectsList() {
+            return axios.get('http://127.0.0.1:8000/api/projects/');
+        }
+
+        Promise.all([getBiography(), getProjectsList()])
+        .then( (results) => {
+                this.bio = results[0].data
+                this.projects = results[1].data 
+            }
+        )
+        .catch(err => console.log(err))
   }
 }
 </script>
