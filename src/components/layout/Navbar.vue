81339dd77227666668a9005f115538d8a19482e1
Donovan Costa <donovancosta@gmail.com>
2020-07-16 22:58:42 +0100

    - Updated navbar link text

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -10,7 +10,7 @@
             </b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav class="ml-auto">
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item :to="{ name:'Home', hash: '#about' }">About</b-nav-item>
+                    <b-nav-item :to="{ name:'Home', hash: '#about' }">About me</b-nav-item>
                     <b-nav-item :to="{ name:'Home', hash: '#Projects' }">Projects</b-nav-item>
                     <b-nav-item :to="{ name:'Home', hash: '#Contact' }">Contact me</b-nav-item>
                 </b-navbar-nav>
