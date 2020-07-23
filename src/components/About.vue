31bbcc9d9d695dc8d036a101fa8fe3c608156fa1
Donovan Costa <donovancosta@gmail.com>
2020-07-23 21:36:57 +0100

    - Added lead text class to about me - Updated component text color

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
             <b-row justify align-v="center" align-h="center" >
                 <b-col cols="12" class="about_text">
                     <h1>About me</h1>
-                    <div v-html="about_me"></div>
+                    <div class="lead" v-html="about_me"></div>
                 </b-col>
             </b-row>
              <hr>
@@ -29,5 +29,6 @@ export default {
 <style>
     .about{
         padding: 1em 0;
+        color: black;
     }
 </style>
